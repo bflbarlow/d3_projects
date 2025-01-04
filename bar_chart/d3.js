a8790ab239f41310bac5860810f6b919fb22f4f5
bflbarlow <bflbarlow@gmail.com>
2025-01-03 21:01:13 -0600

    Additional Bars code

--- bar_chart/d3.js
+++ bar_chart/d3.js
@@ -16,37 +16,45 @@ d3.csv("https://raw.githubusercontent.com/bflbarlow/d3_projects/refs/heads/main/
 
   // sort data
   data.sort(function(b, a) {
-   return a.Value - b.Value;
+    return a.Value - b.Value;
   });
 
-  // X axis
+  // get max
+  var max = d3.max(data, function(d) { return +d.Value;} );;
+
+  // console.log(max);
+
+  // create X axis
   var x = d3.scaleBand()
-   .range([ 0, width ])
-   .domain(data.map(d => d.Country))
-   .padding(0.2);
+    .range([ 0, width ])
+    .domain(data.map(d => d.Category))
+    .padding(0.2);
 
-  // Add Y axis
+  // create Y axis
   var y = d3.scaleLinear()
-   .domain([0, 13000])
-   .range([ height, 0]);
-  svg.append("g")
-   .call(d3.axisLeft(y));
+    .domain([0, max*1.1])
+    .range([ height, 0]);
 
+  // add Y axis to graph (g)
   svg.append("g")
-   .attr("transform", `translate(0, ${height})`)
-   .call(d3.axisBottom(x))
-   .selectAll("text")
-     .attr("transform", "translate(-10,0)rotate(-45)")
-     .style("text-anchor", "end");
+    .call(d3.axisLeft(y));
 
-  // Bars
+  // add X axis to graph (g) with labels
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // create and add bars
   svg.selectAll("mybar")
-   .data(data)
-   .join("rect")
-     .attr("x", d => x(d.Country))
-     .attr("y", d => y(d.Value))
-     .attr("width", x.bandwidth())
-     .attr("height", d => height - y(d.Value))
-     .attr("fill", "#69b3a2")
+    .data(data)
+    .join("rect")
+      .attr("x", d => x(d.Category))
+      .attr("y", d => y(d.Value))
+      .attr("width", x.bandwidth())
+      .attr("height", d => height - y(d.Value))
+      .attr("fill", "#69b3a2")
 
 })
